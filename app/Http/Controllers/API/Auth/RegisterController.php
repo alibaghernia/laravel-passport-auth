e4fc0060d0b0f4689a64d07e156c449d0a10eff4
Code in dev <65717942+codeindevelopment@users.noreply.github.com>
2020-11-22 21:31:02 +0330

    create auth controllers and installed carbon package

---
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    //
+}
